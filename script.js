b3ec348d0db3d2f906d13afa8085d88eb946189d
Farid Aliyev <farid.aliyevv258@gmail.com>
2024-08-14 12:01:15 +0400

    Commit

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const hamburger = document.querySelector('.menu-toggle');
+    const navLinks = document.querySelector('.nav-links');
+
+    hamburger.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
+});
